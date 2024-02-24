f87b3dd0905555f1c7db6c0b98b269d5d1cd834a
Aman Khinvasara <amankhinvasara@gmail.com>
2024-02-24 14:05:12 -0600

    getLastRoll

---
+++ crypto-monopoly/program/programs/crypto-monopoly/src/instructions/get_last_roll.rs
@@ -0,0 +1,8 @@
+pub use crate::errors::*;
+use crate::state::game_data::GameData;
+use anchor_lang::prelude::*;
+use crate::instructions::whose_turn::WhoseTurn;
+
+pub fn get_last_roll(ctx: Context<WhoseTurn>) -> Result<u8> {
+    return Ok(ctx.accounts.game_data.lastRoll());
+}

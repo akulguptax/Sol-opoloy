96b2b6062b3d0e6915761926307690d675821420
Aman Khinvasara <amankhinvasara@gmail.com>
2024-02-24 22:17:50 -0600

    start_turn_simplfiy

--- crypto-monopoly/program/programs/crypto-monopoly/src/instructions/start_turn.rs
+++ crypto-monopoly/program/programs/crypto-monopoly/src/instructions/start_turn.rs
@@ -6,9 +6,9 @@ use anchor_lang::prelude::*;
 pub fn start_turn(ctx: Context<InitPlayer>) -> Result<MoveResult> {
     let player = ctx.accounts.signer.key();
     let p = ctx.accounts.game_data.getPlayerIndex(&player)?;
-    if ctx.accounts.game_data.state != State::PreRoll || 
-        ctx.accounts.game_data.turn as u8 != p {
-        return err!(GameErrorCode::NotYourTurn);
-    }
+    // if ctx.accounts.game_data.state != State::PreRoll || 
+    //     ctx.accounts.game_data.turn as u8 != p {
+    //     return err!(GameErrorCode::NotYourTurn);
+    // }
     return Ok(ctx.accounts.game_data.startTurn(p));
 }
